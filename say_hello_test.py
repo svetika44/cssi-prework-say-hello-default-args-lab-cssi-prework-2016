c4c8799768390da4ddae5edd97311a489ce1ae08
dfenjves <fenjves.daniel@gmail.com>
2016-03-22 13:02:46 -0400

    python converted tests

---
+++ say_hello_test.py
@@ -0,0 +1,18 @@
+import sys
+import unittest
+
+from say_hello import *
+
+class TestMorning(unittest.TestCase):
+
+    def test_say_hello_with_args(self):
+        self.assertEqual(say_hello("Haviva"), "Hello Haviva!")
+        self.assertEqual(say_hello("Ricardo"), "Hello Ricardo!")
+        self.assertEqual(say_hello("Larry"), "Hello Larry!")
+
+    def def test_say_hello_with_args(self):
+        self.assertEqual(say_hello(), "Hello Python Programmer!")
+
+
+if __name__ == '__main__':
+    unittest.main()
